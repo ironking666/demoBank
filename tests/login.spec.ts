78ca9f6d359384af6b677d7aa0b36ed68595cb43
Krzysztof Waliszewski <krzysztof.waliszewski87@gmail.com>
2023-09-04 14:30:25 +0200

    first aaa implementation

--- tests/login.spec.ts
+++ tests/login.spec.ts
@@ -1,13 +1,18 @@
 import { test, expect } from "@playwright/test";
 test.describe("User login to Demobank", () => {
   test("successful login with correct credentials", async ({ page }) => {
-    await page.goto("https://demo-bank.vercel.app/");
-    await page.getByTestId("login-input").fill("testerLO");
+    const url = "https://demo-bank.vercel.app/";
+    const userId = "testerLO";
+    const userPassword = "Tester66";
+    const expectedUserName = "Jan Demobankowy";
+
+    await page.goto(url);
+    await page.getByTestId("login-input").fill(userId);
     await page.getByTestId("login-input").press("Tab");
-    await page.getByTestId("password-input").fill("Tester66");
+    await page.getByTestId("password-input").fill(userPassword);
     await page.getByTestId("login-button").click();
 
-    await expect(page.getByTestId("user-name")).toHaveText("Jan Demobankowy");
+    await expect(page.getByTestId("user-name")).toHaveText(expectedUserName);
   });
 
   test("unsuccessful login with too short username", async ({ page }) => {
--- tests/pulpit.spec.ts
+++ tests/pulpit.spec.ts
@@ -1,20 +1,31 @@
 import { test, expect } from "@playwright/test";
 test.describe("Pulpit tests", () => {
   test("quick payment with correct data", async ({ page }) => {
-    await page.goto("https://demo-bank.vercel.app/");
-    await page.getByTestId("login-input").fill("testerLO");
-    await page.getByTestId("password-input").fill("TesterLO");
+
+    const url = "https://demo-bank.vercel.app/";
+    const userId = "testerLO";
+    const userPassword = "Tester66";
+
+    const receiverId = "2";
+    const transferAmount = "150";
+    const transferTitle = "Zwrot funduszy";
+    const expectedTransferReceiver = 'Chuck Demobankowy';
+
+    
+    await page.goto(url);
+    await page.getByTestId("login-input").fill(userId);
+    await page.getByTestId("password-input").fill(userPassword);
     await page.getByTestId("login-button").click();
-    await page.locator("#widget_1_transfer_receiver").selectOption("2");
+    await page.locator("#widget_1_transfer_receiver").selectOption(receiverId);
     await page.locator("#widget_1_transfer_amount").click();
-    await page.locator("#widget_1_transfer_amount").fill("150");
+    await page.locator("#widget_1_transfer_amount").fill(transferAmount);
     await page.locator("#widget_1_transfer_title").click();
-    await page.locator("#widget_1_transfer_title").fill("Zwrot funduszy");
+    await page.locator("#widget_1_transfer_title").fill(transferTitle);
     await page.getByRole("button", { name: "wykonaj" }).click();
     await page.getByTestId("close-button").click();
 
     await expect(page.locator("#show_messages")).toHaveText(
-      "Przelew wykonany! Chuck Demobankowy - 150,00PLN - Zwrot funduszy"
+      `Przelew wykonany! ${expectedTransferReceiver} - ${transferAmount},00PLN - ${transferTitle}`
     );
   });
 
