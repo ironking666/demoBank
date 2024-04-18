f5f56bc858e7fa36627391abc09f421ed0945a4f
Krzysztof Waliszewski <47249823+ironking666@users.noreply.github.com>
2024-04-18 13:47:32 +0200

    Update login.spec.ts

--- tests/login.spec.ts
+++ tests/login.spec.ts
@@ -12,7 +12,7 @@ test.describe("User login to Demobank", () => {
   test("successful login with correct credentials", async ({ page }) => {
     const userId = loginData.userId;
     const userPassword = loginData.userPassword;
-    const expectedUserName = "Jan Demobankowy";
+    const expectedUserName = "Jan Demobankowy 1122";
 
     await loginPage.login(userId, userPassword);
 
